63d1ccaa993d341143e10c8103ac130a8c4d6320
Michal Durkovic <michal.durkovic@innovatrics.com>
2019-07-24 12:36:32 +0200

    Update url in conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -75,7 +75,7 @@ class BoostConan(ConanFile):
     default_options.append("without_python=True")
     default_options = tuple(default_options)
 
-    url = "https://github.com/lasote/conan-boost"
+    url = "https://github.com/midurk/conan-boost"
     license = "Boost Software License - Version 1.0. http://www.boost.org/LICENSE_1_0.txt"
     short_paths = True
     no_copy_source = True
